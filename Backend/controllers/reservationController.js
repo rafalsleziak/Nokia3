f4d237333a582bcaee7a7ceeee6a9b4421285d4e
Rafal Sleziak <226390@student.pwr.edu.pl>
2018-04-03 20:30:23 +0200

    Your Message

---
+++ Backend/controllers/reservationController.js
@@ -0,0 +1,49 @@
+var Device = require('../models/device');
+var Reservation=require('../models/reservations');
+exports.index = function(req, res, next) {
+  res.json({express: 'API works!'});
+  console.log('hello there');
+}
+
+
+export.reservation_list = function (req, res, next) {
+  Reservation.find(function(err reservations)) {
+    if (err)
+    res.send(err);
+    res.json(reservation);
+
+    export.index=function(res,req) {
+      req.render('index');
+    }
+  }
+}
+
+exports.reservation_list = function(req, res) {
+  models.reservations.findAll({  }).then((result) => {
+  });
+};
+
+export.reservation_detail=function(req,res) {
+models.reservations.findOne ({where:{id:req.params.id }}.then(res))
+
+}
+
+exports.reservation_create_get = function(req, res) {
+
+
+}
+
+exports.device_create_post = function(req, res) {
+  var reservation = new Reservation();
+  (req.body.startDate) ? reservation.startDate = req.body.startDate : null;
+  (req.body.endDate) ? reservation.endDate= req.body.endDate : null;
+    (req.body.NumOfPeople) ? reservation.NumOfPeople= req.body.NumOfPeople : null;
+    (req.body.Options) ? reservation.Options= req.body.Options : null;
+
+
+  reservation.save(function(err) {
+    if (err)
+      res.send(err);
+    res.json({ message: 'Reservation successfully added!' });
+  });
+}
---
+++ Backend/models/reservations.js
@@ -0,0 +1,51 @@
+'use strict'
+
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var ReservationSchema =new Schema( {
+  startDate: {
+    type: Date,
+    required: true
+   },
+  endDate: {
+    type: Date,
+    required: true
+   },
+  numOfPeople: {
+    type: Number,
+    required: true,
+    min: 0,
+    max: 12
+  },
+  option: {
+    type: String,
+    enum: ['MakerSpace', 'OpenSpace', 'WholeSpace'],
+    required: true
+  }
+});
+
+ReservationSchema.virtual('Reservation_startDate').get(function() {
+  return this.startDate;
+});
+
+ReservationSchema.virtual('Reservation_endDate').get(function() {
+  return this.endDate;
+});
+
+ReservationSchema.virtual('Reservation_numOfPeople').get(function() {
+  return this.numOfPeople;
+});
+
+ReservationSchema.virtual('Reservation_option').get(function() {
+  return this.option;
+});
+
+ReservationSchema
+.virtual('url')
+.get(function(){
+  return '/api/reservations/'+ this._id;
+});
+
+module.exports = mongoose.model('Reservation', ReservationSchema);
